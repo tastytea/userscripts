991e8a07106f452b639aac5583a3322462e3962a
tastytea <tastytea@tastytea.de>
2019-06-07 14:49:51 +0200

    Added german description.

--- comment-sections/rbb24_expand_comments.user.js
+++ comment-sections/rbb24_expand_comments.user.js
@@ -1,18 +1,19 @@
 // ==UserScript==
-// @name        Expand rbb24.de comments
-// @description Expand comments under articles on rbb24.de.
-// @version     2019.06.07.1
-// @author      tastytea
-// @copyright   2019, tastytea (https://tastytea.de/)
-// @license     GPL-3.0-only
-// @namespace   tastytea.de
-// @homepageURL https://schlomp.space/tastytea/userscripts
-// @supportURL  https://schlomp.space/tastytea/userscripts/issues
-// @downloadURL https://schlomp.space/tastytea/userscripts/raw/branch/main/comment-sections/rbb24_expand_comments.user.js
-// @grant       none
-// @match       https://*.rbb24.de/*/beitrag/*
-// @run-at      document-end
-// @inject-into content
+// @name           Expand rbb24.de comments
+// @description    Expands comments under articles on rbb24.de.
+// @description:de Klappt kommentare unter artikeln auf rbb24.de aus.
+// @version        2019.06.07.2
+// @author         tastytea
+// @copyright      2019, tastytea (https://tastytea.de/)
+// @license        GPL-3.0-only
+// @namespace      tastytea.de
+// @homepageURL    https://schlomp.space/tastytea/userscripts
+// @supportURL     https://schlomp.space/tastytea/userscripts/issues
+// @downloadURL    https://schlomp.space/tastytea/userscripts/raw/branch/main/comment-sections/rbb24_expand_comments.user.js
+// @grant          none
+// @match          https://*.rbb24.de/*/beitrag/*
+// @run-at         document-end
+// @inject-into    content
 // ==/UserScript==
 
 function expand_comments()

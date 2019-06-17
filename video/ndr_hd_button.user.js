a28fe215e34eb80be55ad8f0922613132a5d40e5
tastytea <tastytea@tastytea.de>
2019-06-17 13:12:41 +0200

    Added deprecation warning to ndr_hd_button.

---
+++ video/ndr_hd_button.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           NDR HD button
+// @description    DEPRECATED
+// @version        2019.06.17.1
+// @author         tastytea
+// @copyright      2019, tastytea (https://tastytea.de/)
+// @license        GPL-3.0-only
+// @namespace      tastytea.de
+// @homepageURL    https://schlomp.space/tastytea/userscripts
+// @supportURL     https://schlomp.space/tastytea/userscripts/issues
+// @downloadURL    https://schlomp.space/tastytea/userscripts/raw/branch/main/video/ndr_hd_button.user.js
+// @grant          none
+// @match          https://*.ndr.de/*
+// @run-at         document-end
+// @inject-into    content
+// ==/UserScript==
+
+alert("The userscript “NDR HD button” is deprecated. Please use “NDR download button” instead.\n"
+      + "https://openuserjs.org/scripts/tastytea/NDR_download_button");

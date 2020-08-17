acfde2841985009649fa92f5ba4a8f2269e0e506
tastytea <tastytea@tastytea.de>
2020-08-18 00:30:13 +0200

    goodreads expand: Also expand author info.

--- catalogues/goodreads_expand.user.js
+++ catalogues/goodreads_expand.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           Goodreads expand
-// @description    Show the whole description on goodreads.com.
-// @description:de Zeige die ganze beschreibung auf goodreads.com an.
-// @version        2020.08.17.1
+// @description    Show the whole description / author information on goodreads.com.
+// @description:de Zeige die ganze beschreibung / author·inneninformation auf goodreads.com an.
+// @version        2020.08.18.1
 // @author         tastytea
 // @copyright      2020, tastytea (https://tastytea.de/)
 // @license        GPL-3.0-only
@@ -12,25 +12,31 @@
 // @downloadURL    https://schlomp.space/tastytea/userscripts/raw/branch/main/catalogues/goodreads_expand.user.js
 // @grant          none
 // @match          https://www.goodreads.com/book/*
+// @match          https://www.goodreads.com/author/*
 // @run-at         document-end
 // @inject-into    content
 // ==/UserScript==
 
 (function()
  {
-     const description = document.getElementById("description");
-     if (description !== null)
+     let root = document.getElementById("description");
+     if (root === null)
      {
-         const spans = description.getElementsByTagName("span");
-         if (spans.length >= 2)
+         root = document.getElementsByClassName("aboutAuthorInfo")[0];
+         if (root === null)
          {
-             spans[1].setAttribute("style", "display: block;");
-             spans[0].setAttribute("style", "display: none;");
-             description.getElementsByTagName("a")[0].text = "(less)";
+             console.warn("Could not find description / author info.");
+             return;
          }
      }
-     else
+
+     const spans = root.getElementsByTagName("span");
+     if (spans.length >= 2)
      {
-         console.warn("Could not find description.");
+         spans[1].setAttribute("style", "display: block;");
+         spans[0].setAttribute("style", "display: none;");
+
+         const links = root.getElementsByTagName("a");
+         links[links.length - 1].text = "(less)";
      }
  })();

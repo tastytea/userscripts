c555e69ad1da6169647bc98860e76644ccb88877
tastytea <tastytea@tastytea.de>
2021-04-20 09:47:44 +0200

    Goodreads expand: Expand book details too.

--- catalogues/goodreads_expand.user.js
+++ catalogues/goodreads_expand.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name           Goodreads expand
-// @description    Show the whole description / author information on goodreads.com.
-// @description:de Zeige die ganze beschreibung / author·inneninformation auf goodreads.com an.
-// @version        2020.08.18.1
+// @description    Show the whole description / author information / book details on goodreads.com.
+// @description:de Zeige die ganze beschreibung / author·inneninformation / buchdetails auf goodreads.com an.
+// @version        2021.04.20.1
 // @author         tastytea
-// @copyright      2020, tastytea (https://tastytea.de/)
+// @copyright      2020, 2021, tastytea (https://tastytea.de/)
 // @license        GPL-3.0-only
 // @namespace      tastytea.de
 // @homepageURL    https://schlomp.space/tastytea/userscripts
@@ -38,5 +38,12 @@
 
          const links = root.getElementsByTagName("a");
          links[links.length - 1].text = "(less)";
+     }
+
+     const bookdata = document.getElementById("bookDataBox");
+     if (bookdata !== null)
+     {
+         bookdata.setAttribute("style", "display: block;");
+         document.getElementById("bookDataBoxShow").setAttribute("style", "display: none;");
      }
  })();
